faa78ed7a6a7bc7f7e7dc67ac1b3465a75ab2b20
hwang <hgh1057@gmail.com>
2023-07-07 22:29:25 +0900

    feat:공용 컴포넌트 흰 박스 및 기초세팅

--- src/components/BackGround/BackGround.tsx
+++ src/components/BackGround/BackGround.tsx
@@ -1,3 +1,27 @@
-export default function BackGround() {
-  return <div>hi</div>;
+import { styled } from "styled-components";
+
+type TitleProps = {
+  title: string;
+};
+
+export default function BackGround(props: TitleProps) {
+  return (
+    <Box>
+      <Title>{props.title}</Title>
+    </Box>
+  );
 }
+
+const Box = styled.div`
+  margin: 5.81rem 7.5rem 0 7.5rem;
+  background: white;
+  border-radius: 3.75rem 3.75rem 0 0;
+  height: 100vh;
+  padding: 1.88rem;
+`;
+
+const Title = styled.p`
+  font-size: 2.25rem;
+
+  text-align: center;
+`;
