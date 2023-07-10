efb926c5d94f917adc7356c4709b18ebede93a57
hwang <hgh1057@gmail.com>
2023-07-10 23:52:52 +0900

    fix 충돌땜에 바꿈

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { ThemeProvider } from "styled-components";
-import GlobalStyle from "./styles/GlobalStyle";
-import { theme } from "./styles/theme";
-// import MainPage from './pages/main';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import { theme } from './styles/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      {/* <MainPage /> */}
     </ThemeProvider>
   );
 }
