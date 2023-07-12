da0c982552d3e218cd00091d5b96a1bc3da1da63
egg-silver <v6984@naver.com>
2023-07-12 15:10:17 +0900

    feat:로딩 스피너 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import GlobalStyle from './styles/GlobalStyle';
 import { theme } from './styles/theme';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/Main';
+import Loading from './components/Loading/Loading';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Loading title={'캐릭터를 만들고 있어요!'} />
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/login' element={<></>} />
