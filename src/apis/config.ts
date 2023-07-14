5f89acf581e04da451bea2e140c656183f995a14
hwang <hgh1057@gmail.com>
2023-07-14 22:45:01 +0900

    chore baseURL

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'http://3.35.88.150:8080', // 기본 URL 설정
+const BASE_URL = 'http://3.35.88.150:8080';
+
+export const baseInstance = axios.create({
+  baseURL: BASE_URL, // 기본 URL 설정
 });
 
-//axios 대신 api 사용하면 됩니다.
+//axios대신 baseInstance
+
+//authInstance
