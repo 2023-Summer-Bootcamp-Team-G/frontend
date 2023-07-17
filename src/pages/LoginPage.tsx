f6ada6f9962515971161024038a2f6ebcd5df980
woohee <aldkgha@naver.com>
2023-07-17 14:45:09 +0900

    test

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,18 +1,37 @@
 import { styled } from 'styled-components';
 import LoginInput from '../components/Input/LoginInput';
 import RoundButton from '../components/Btn/RoundBtn';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { animate, motion } from 'framer-motion';
 import { opacityVariants } from '../constants/variants';
+import { useState } from 'react';
+import { baseInstance } from '../apis/config';
 
 export default function LoginPage() {
+  const [id, setId] = useState('');
+  const [passwd, setPasswd] = useState('');
+  const navigate = useNavigate();
+
+  const createUser = async (e: any) => {
+    e.preventDefault();
+    const data = { user_id: id, password: passwd };
+    const response = await baseInstance.post('/login', data);
+    console.log(response.data);
+
+    if (response.status === 201) {
+      navigate('/login');
+    } else alert('다시 작성해주세요');
+
+    console.log(response.data);
+  };
+
   return (
     <Layout>
       <Box variants={opacityVariants} initial='initial' animate='mount'>
         <h1>로그인</h1>
         <LoginInput title='아이디' placeholder='영문 + 숫자' />
         <LoginInput title='비밀번호' placeholder='6자리 이상' />
-        <RoundButton title='로그인' />
+        <RoundButton onClick={createUser} title='로그인' />
         <br />
         <Link to='/signup'>회원가입 하러가기</Link>
       </Box>
