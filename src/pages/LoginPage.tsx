c280b0539a3343fe06e87dabf02977cb9e667841
woohee <aldkgha@naver.com>
2023-07-24 01:36:10 +0900

    design: login, signup 퍼블리싱 수정 및 애니메이션 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import { styled } from 'styled-components';
 import LoginInput from '../components/Input/LoginInput';
 import RoundButton from '../components/Btn/RoundBtn';
 import { Link, useNavigate } from 'react-router-dom';
-import { animate, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { opacityVariants } from '../constants/variants';
 import { useState } from 'react';
 import { baseInstance } from '../apis/config';
@@ -33,61 +33,71 @@ export default function LoginPage() {
   };
 
   return (
-    <Layout>
-      <Box variants={opacityVariants} initial='initial' animate='mount'>
-        <h1>로그인</h1>
-        <LoginInput
-          title='아이디'
-          placeholder='영문 + 숫자'
-          value={id}
-          setValue={setId}
-          type='text'
-        />
-        <LoginInput
-          title='비밀번호'
-          placeholder='6자리 이상'
-          value={passwd}
-          setValue={setPasswd}
-          type='password'
-        />
-        <RoundButton onClick={createUser} title='로그인' />
-        <br />
-        <Link to='/signup'>회원가입 하러가기</Link>
+    <BackLayout>
+      <Box>
+        <WhiteBox variants={opacityVariants} initial='initial' animate='mount'>
+          <h1>로그인</h1>
+          <LoginInput
+            title='아이디'
+            placeholder='영문 + 숫자'
+            value={id}
+            setValue={setId}
+            type='text'
+          />
+          <LoginInput
+            title='비밀번호'
+            placeholder='6자리 이상'
+            value={passwd}
+            setValue={setPasswd}
+            type='password'
+          />
+          <RoundButton onClick={createUser} title='로그인' />
+          <br />
+          <StyledLink to='/signup'>회원가입 하러가기</StyledLink>
+        </WhiteBox>
+        <ImgLayout>
+          <Img1 src='https://i.postimg.cc/brLtCYTf/3.png' />
+          <Img2 src='https://i.postimg.cc/524P2jGL/2.png' />
+        </ImgLayout>
       </Box>
-      <ImgLayout>
-        <Img1 src='https://i.postimg.cc/brLtCYTf/3.png' />
-        <Img2 src='https://i.postimg.cc/524P2jGL/2.png' />
-      </ImgLayout>
-    </Layout>
+    </BackLayout>
   );
 }
-
+const StyledLink = styled(Link)`
+  margin-right: 2rem;
+`;
+const Box = styled.div`
+  margin: 3.37rem;
+  display: flex;
+`;
 const ImgLayout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-left: 8rem;
-  /* justify-content: center; */
+  margin-left: 2rem;
+  justify-content: center;
 `;
 const Img1 = styled.img`
-  width: 15.5rem;
-  height: 15.5rem;
+  width: 15rem;
+  height: 15rem;
   position: relative;
-  left: 6rem;
+  margin-left: 9rem;
 `;
 const Img2 = styled.img`
-  width: 25.5rem;
-  height: 25.5rem;
+  width: 30rem;
+  height: 30rem;
 `;
 
-const Layout = styled.div`
-  padding: 3.5rem;
+const BackLayout = styled.div`
+  /* padding: 3.5rem; */
   display: flex;
   background: linear-gradient(#ff6600, #ffc301);
 `;
-const Box = styled(motion.div)`
+const WhiteBox = styled(motion.div)`
   width: 42rem;
   height: 43rem;
+  margin-top: 3rem;
+  margin-left: 9rem;
   background: white;
   border-radius: 0.94rem;
   display: flex;
