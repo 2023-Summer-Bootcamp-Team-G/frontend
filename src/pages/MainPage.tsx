d6ca248aee57ec181e6a83db7121a5fc683d457f
woohee <aldkgha@naver.com>
2023-07-24 23:35:09 +0900

    design: mainpage 퍼블리싱 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,12 +3,7 @@ import styled from 'styled-components';
 import Button from '../components/Btn/Btn';
 import { keyframes } from 'styled-components';
 
-const blinkAnimation = `
-  0% { opacity: 1; }
-  50% { opacity: 0; }
-  100% { opacity: 1; }
-`;
-
+// 애니메이션 효과 왜 적용 안되는 지는 나중에,,,
 // 위치 변경 애니메이션
 const moveAnimation = keyframes`
   0% { transform: translate(0, 0); }
@@ -16,6 +11,12 @@ const moveAnimation = keyframes`
   50% { transform: translate(-20%, 10%); }
   75% { transform: translate(20%, -10%); }
   100% { transform: translate(-10%, -20%); }
+`;
+
+const blinkAnimation = `
+  0% { opacity: 1; }
+  50% { opacity: 0; }
+  100% { opacity: 1; }
 `;
 
 const StyledImg = styled.img`
@@ -29,100 +30,99 @@ const StyledImg = styled.img`
 
 export default function MainPage() {
   return (
-    <ParentBox>
-      <TextLayout>
-        <Text>이게 나라고?</Text>
+    <Container>
+      <BackLayout>
+        <TextLayout>
+          <Text>이게 나라고?</Text>
+          <img
+            style={{
+              width: '17rem',
+              height: '6rem',
+            }}
+            src='https://i.postimg.cc/hhWXxq4Y/image-6.png'
+            alt='이미지'
+          />
+        </TextLayout>
+
+        <StyledImg
+          style={{
+            position: 'absolute',
+            top: '0',
+            left: '-1.25rem',
+            width: '30rem',
+            height: '30rem',
+          }}
+          src='https://i.postimg.cc/m20Hmb7K/2.png'
+          alt='토끼'
+          className='blink'
+        />
+        <StyledImg
+          style={{
+            position: 'absolute',
+            bottom: '-5.5rem',
+            left: '0',
+            clear: 'both',
+            width: '30rem',
+            height: '30rem',
+          }}
+          src='https://i.postimg.cc/3wZ0rmzH/3.png'
+          alt='고래'
+          className='blink'
+        />
         <img
           style={{
-            width: '17rem',
-            height: '6rem',
+            position: 'relative',
+            width: '44rem',
+            height: '44rem',
+            top: '38%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
           }}
-          src='https://i.postimg.cc/hhWXxq4Y/image-6.png'
-          alt='이미지'
+          src='https://i.postimg.cc/V6GVj5FZ/1.png'
+          alt='여우'
         />
-      </TextLayout>
-
-      {/* <ImgLayout> */}
-      {/* <CharLayout> */}
-      <StyledImg
-        style={{
-          position: 'absolute',
-          top: '0',
-          left: '-1.25rem',
-          width: '30rem',
-          height: '30rem',
-        }}
-        src='https://i.postimg.cc/m20Hmb7K/2.png'
-        alt='토끼'
-        className='blink'
-      />
-      <StyledImg
-        style={{
-          position: 'absolute',
-          bottom: '-100px',
-          left: '0',
-          clear: 'both',
-          width: '30rem',
-          height: '30rem',
-        }}
-        src='https://i.postimg.cc/3wZ0rmzH/3.png'
-        alt='고래'
-        className='blink'
-      />
-      {/* </CharLayout> */}
-      <img
-        style={{
-          position: 'relative',
-          width: '40rem',
-          height: '40rem',
-          // margin: '17.5rem 20rem 0 20rem',
-          top: '38%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          zIndex: 0,
-        }}
-        src='https://i.postimg.cc/V6GVj5FZ/1.png'
-        alt='여우'
-      />
-      {/* <CharLayout> */}
-      <StyledImg
-        style={{
-          position: 'absolute',
-          top: '0',
-          right: '0',
-          width: '30rem',
-          height: '30rem',
-        }}
-        src='https://i.postimg.cc/DZyJSmK4/4.png'
-        alt='고양이'
-        className='blink'
-      />
-      <StyledImg
-        style={{
-          position: 'absolute',
-          bottom: '-100px',
-          right: '0',
-          clear: 'both',
-          width: '30rem',
-          height: '30rem',
-        }}
-        src='https://i.postimg.cc/nLxQt0Rk/5.png'
-        alt='새'
-        className='blink'
-      />
-      {/* </CharLayout> */}
-      {/* </ImgLayout> */}
-      <Link to='/login'>
-        <StyledButton title={'바로 시작하기 >'}></StyledButton>
-      </Link>
-    </ParentBox>
+        <StyledImg
+          style={{
+            position: 'absolute',
+            top: '-1.3rem',
+            right: '0',
+            width: '30rem',
+            height: '30rem',
+          }}
+          src='https://i.postimg.cc/DZyJSmK4/4.png'
+          alt='고양이'
+          className='blink'
+        />
+        <StyledImg
+          style={{
+            position: 'absolute',
+            bottom: '-2.5rem',
+            right: '0',
+            clear: 'both',
+            width: '30rem',
+            height: '30rem',
+          }}
+          src='https://i.postimg.cc/nLxQt0Rk/5.png'
+          alt='새'
+          className='blink'
+        />
+        <ButtonLayout>
+          <Link to='/login'>
+            <Button title={'바로 시작하기 >'}></Button>
+          </Link>
+        </ButtonLayout>
+      </BackLayout>
+    </Container>
   );
 }
-
-const ParentBox = styled.div`
+const BackLayout = styled.div`
   position: relative;
-  height: 96vh;
+  background: linear-gradient(#ff6600, #ffc301);
+  height: 100vh;
+  overflow: hidden;
+  width: 100%;
 `;
+
 const Text = styled.div`
   color: #fff;
   font-size: 2rem;
@@ -137,21 +137,22 @@ const TextLayout = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin-left: 3rem;
+  top: 5rem;
 `;
-// const CharLayout = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   position: absolute;
-// `;
-// const ImgLayout = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-// `;
-const StyledButton = styled(Button)`
-  /* position: absolute; */
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 1;
+
+const ButtonLayout = styled.div`
+  left: 36rem;
+  justify-content: center;
+  position: absolute;
+  bottom: 3.87rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 `;
