f8ff1752f5e31283aa9742b38d65145cafb607aa
hwang <hgh1057@gmail.com>
2023-07-26 11:22:27 +0900

    chore : proxy설정 및 config재설정

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://3.35.88.150:8000/api';
+const BASE_URL =
+  import.meta.env.MODE === 'development'
+    ? '/api'
+    : 'http://3.35.88.150:8000/api';
 
 export const baseInstance = axios.create({
+  // withCredentials: true,
   baseURL: BASE_URL, // 기본 URL 설정
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://3.35.88.150:8000',
+        // changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '/api/'),
+        // secure: false,
+        // ws: true,
+      },
+    },
+  },
+});
