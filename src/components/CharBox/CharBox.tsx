4608deb3d440cc6cf8ca5728be5c7787efdc3eeb
egg-silver <v6984@naver.com>
2023-07-26 14:57:51 +0900

    feat:디테일 페이지 api 연결

--- src/components/CharBox/CharBox.tsx
+++ src/components/CharBox/CharBox.tsx
@@ -2,15 +2,17 @@ import styled from 'styled-components';
 
 type CharBoxProps = {
   imageURL: string;
+  onClick: any;
 };
 
-export default function CharBox({ imageURL }: CharBoxProps) {
+export default function CharBox({ imageURL, onClick }: CharBoxProps) {
   return (
     <Box>
       <img
         style={{ width: '13rem', height: '13rem' }}
         src={imageURL}
         alt='Character'
+        onClick={onClick}
       />
     </Box>
   );
@@ -19,5 +21,6 @@ export default function CharBox({ imageURL }: CharBoxProps) {
 const Box = styled.div`
   width: 13rem;
   height: 13rem;
+
   //   background-color: red; // 위치 잘 보기위해서 넣은 색
 `;
